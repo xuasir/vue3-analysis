f19d0714c571f2b341a3e639851c1ca34abb5754
guo.xu <xuguo@outlook.it>
2021-03-10 14:02:40 +0800

    docs: 修改vue-reuse链接

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,56 +1,57 @@
 module.exports = {
-  base: '/vue3-analysis/',
-  dest: 'dist',
-  title: 'vue3解析',
-  description: ' ',
-  plugins: ['@vuepress/back-to-top'],
+  base: "/vue3-analysis/",
+  dest: "dist",
+  title: "vue3解析",
+  description: " ",
+  plugins: ["@vuepress/back-to-top"],
   themeConfig: {
-    repo: 'xuguo-code/vue3-analysis',
-    logo: '/logo.png',
+    repo: "xuguo-code/vue3-analysis",
+    logo: "/logo.png",
     editLinks: false,
-    docsDir: './',
-    lastUpdated: '最后一次更新',
+    docsDir: "./",
+    lastUpdated: "最后一次更新",
     nav: [
-      {text: 'Vue3', link: '/v3/idea/vue'},
-      {text: 'vue-hooks', link: 'http://xuguo.xyz/vue-hooks'},
-      {text: '我要修正', link: 'https://github.com/xuguo-code/vue3-analysis/issues'},
+      { text: "Vue3", link: "/v3/idea/vue" },
+      { text: "vue-reuse", link: "http://xuguo.xyz/vue-reuse" },
+      {
+        text: "我要修正",
+        link: "https://github.com/xuguo-code/vue3-analysis/issues",
+      },
     ],
     sidebar: {
-      '/v3/': [
+      "/v3/": [
         {
-          title: '理念篇',
+          title: "理念篇",
           collapsable: true,
           children: [
             // 'idea/',
-            'idea/vue',
-            'idea/UI',
-            'idea/view',
-            'idea/vue3',
-            'idea/summary'
-          ]
+            "idea/vue",
+            "idea/UI",
+            "idea/view",
+            "idea/vue3",
+            "idea/summary",
+          ],
         },
         {
-          title: '运行时篇',
+          title: "运行时篇",
           collapsable: false,
           children: [
-            ['runtime/', '前言'],
+            ["runtime/", "前言"],
             {
-              title: '组件系统',
+              title: "组件系统",
               collapsable: true,
               children: [
-                'runtime/createApp',
-                'runtime/mount',
-                'runtime/update',
-                'runtime/setup',
-                'runtime/lifecycle'
-              ]
+                "runtime/createApp",
+                "runtime/mount",
+                "runtime/update",
+                "runtime/setup",
+                "runtime/lifecycle",
+              ],
             },
             {
-              title: '异步调度',
+              title: "异步调度",
               collapsable: true,
-              children: [
-                'runtime/scheduler'
-              ]
+              children: ["runtime/scheduler"],
             },
             // {
             //   title: 'VNode',
@@ -58,35 +59,35 @@ module.exports = {
             //   children: [
             //   ]
             // },
-          ]
+          ],
         },
         {
-          title: '响应式系统篇',
+          title: "响应式系统篇",
           collapsable: false,
           children: [
-            ['reactivity/', '前言'],
+            ["reactivity/", "前言"],
             {
-              title: '核心方法',
+              title: "核心方法",
               collapsable: true,
               children: [
-                'reactivity/reactivity',
-                'reactivity/effect',
-                'reactivity/computed',
-                'reactivity/watch',
-                'reactivity/provide'
-              ]
+                "reactivity/reactivity",
+                "reactivity/effect",
+                "reactivity/computed",
+                "reactivity/watch",
+                "reactivity/provide",
+              ],
             },
             {
-              title: '更多方法',
+              title: "更多方法",
               collapsable: true,
               children: [
-                'reactivity/customRef',
-                'reactivity/readonly',
-                'reactivity/shallow',
-                'reactivity/reactivity-utils',
-              ]
+                "reactivity/customRef",
+                "reactivity/readonly",
+                "reactivity/shallow",
+                "reactivity/reactivity-utils",
+              ],
             },
-          ]
+          ],
         },
         // {
         //   title: 'compiler篇',
@@ -96,30 +97,27 @@ module.exports = {
         //   ]
         // },
         {
-          title: '扩展篇',
+          title: "扩展篇",
           collapsable: false,
           children: [
-            ['future/', '前言'],
+            ["future/", "前言"],
             {
-              title: '实用特性',
+              title: "实用特性",
               collapsable: true,
               children: [
-                'future/props',
-                'future/slot',
-                'future/refs',
-                'future/directives',
-                'future/defineAsyncComponent'
-              ]
+                "future/props",
+                "future/slot",
+                "future/refs",
+                "future/directives",
+                "future/defineAsyncComponent",
+              ],
             },
             {
-              title: '内建组件',
+              title: "内建组件",
               collapsable: true,
-              children: [
-                'future/suspense',
-                'future/teleport'
-              ]
-            }
-          ]
+              children: ["future/suspense", "future/teleport"],
+            },
+          ],
         },
         // {
         //   title: '生态篇',
@@ -128,7 +126,7 @@ module.exports = {
         //     ['ecology/', '前言'],
         //   ]
         // },
-      ]
-    }
-  }
-}
+      ],
+    },
+  },
+};
